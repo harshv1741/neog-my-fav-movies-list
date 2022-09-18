9267305445c298b10c5e3d7b9cd857c5abe2f6e4
Harsh Vyapari <harshv1741@gmail.com>
2022-09-18 05:44:07 +0000

    Added some changes

--- src/App.js
+++ src/App.js
@@ -53,8 +53,10 @@ export default function App() {
           paddingRight: "1rem"
         }}
       >
+        <h2 style={{}}>
         Here are some of my favourite movies list which I recommend everyone
         should watch.
+        </h2>            
       </h3>
       {/* Custom Style Buttons (START) */}
       {Object.keys(movieList).map((genre) => (
